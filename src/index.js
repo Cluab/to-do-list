c07364be3f9a16cc62d07d18a277be04fb52cfe4
cluab <cluab11222@gmail.com>
2022-11-01 19:17:08 +0200

    fix linters errors

--- src/index.js
+++ src/index.js
@@ -1,30 +1,29 @@
-import _ from "lodash";
-import "./style.css";
+import './style.css';
 
-let tasks = () => {
-  let list = [
+const tasks = () => {
+  const list = [
     {
-      description: "fight bad guys",
+      description: 'fight bad guys',
       completed: true,
       index: 0,
     },
     {
-      description: "complete project",
+      description: 'complete project',
       completed: false,
       index: 1,
     },
     {
-      description: "learn sass",
+      description: 'learn sass',
       completed: false,
       index: 2,
     },
   ];
-  let mission = document.getElementById("mission");
+  const mission = document.getElementById('mission');
   list.forEach((task) => {
-    let li = document.createElement("li");
-    let checkbox = document.createElement("input");
-    let work = document.createElement("p");
-    checkbox.type = "checkbox";
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const work = document.createElement('p');
+    checkbox.type = 'checkbox';
     work.innerText = task.description;
     li.appendChild(checkbox);
     li.appendChild(work);
