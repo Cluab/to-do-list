ac5df113c1d95a56c1eedf071449fc7df5ad417b
cluab <cluab11222@gmail.com>
2022-11-02 22:19:04 +0200

    complete adding add & remove for local storage

--- src/modules/add.js
+++ src/modules/add.js
@@ -10,13 +10,14 @@ export default class ToDoInfo {
   };
 
   static addToList = (list) => {
-    const mission = document.getElementById("mission");
-    const li = document.createElement("li");
-    const checkbox = document.createElement("input");
-    const work = document.createElement("p");
-    checkbox.type = "checkbox";
-    checkbox.setAttribute("id", list.index);
-    checkbox.setAttribute("class", "checkedbox");
+    const mission = document.getElementById('mission');
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    const work = document.createElement('p');
+    checkbox.type = 'checkbox';
+
+    checkbox.setAttribute('id', list.index);
+    checkbox.setAttribute('class', 'checkedbox');
     work.innerText = list.description;
     li.appendChild(checkbox);
     li.appendChild(work);
@@ -24,21 +25,21 @@ export default class ToDoInfo {
   };
 
   static deletetask = () => {
-    const retrieveData = localStorage.getItem("list");
+    const retrieveData = localStorage.getItem('list');
     const data = JSON.parse(retrieveData);
-    const checkbox = document.querySelectorAll(".checkedbox");
+    const checkbox = document.querySelectorAll('.checkedbox');
     let count = 0;
     checkbox.forEach((onebyone) => {
       if (onebyone.checked === true) {
         data[count].completed = onebyone.checked;
         onebyone.parentElement.remove();
-        localStorage.setItem("list", JSON.stringify(data));
+        localStorage.setItem('list', JSON.stringify(data));
 
         console.log(count);
         count += 1;
       } else if (onebyone.checked === false) {
         data[count].completed = onebyone.checked;
-        localStorage.setItem("list", JSON.stringify(data));
+        localStorage.setItem('list', JSON.stringify(data));
         count += 1;
       }
     });
--- src/modules/locolstorage.js
+++ src/modules/locolstorage.js
@@ -1,10 +1,10 @@
 export default class Store {
   static getlist = () => {
     let list;
-    if (localStorage.getItem("list") === null) {
+    if (localStorage.getItem('list') === null) {
       list = [];
     } else {
-      list = JSON.parse(localStorage.getItem("list"));
+      list = JSON.parse(localStorage.getItem('list'));
     }
     return list;
   };
@@ -12,19 +12,27 @@ export default class Store {
   static addlist = (works) => {
     const list = Store.getlist();
     list.push(works);
-    localStorage.setItem("list", JSON.stringify(list));
+    localStorage.setItem('list', JSON.stringify(list));
   };
 
   static removework = () => {
     const list = Store.getlist();
-    console.log(list);
-    list.forEach((lists, index) => {
-      if (lists.completed === true) {
-        console.log("hello");
-        list.splice(index, 1);
+    for (let i = 0; i < list.length; i += 1) {
+      if (list[i].completed === true) {
+        list.splice(i, 1);
+        for (let j = 0; j < list.length; j += 1) {
+          if (list[j].index > 1) {
+            list[j].index = j;
+            console.log(list[j].index);
+          }
+        }
       }
-    });
-
-    localStorage.setItem("list", JSON.stringify(list));
+    }
+    if (list.length === 0 || list[0].completed === true) {
+      list.splice(0, 1);
+    } else if (list.length === 1 || list[1].completed === true) {
+      list.splice(1, 1);
+    }
+    localStorage.setItem('list', JSON.stringify(list));
   };
 }
