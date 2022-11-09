c70ff666893a834a1867fa9a57eaf0d6fef9a994
cluab <cluab11222@gmail.com>
2022-11-09 14:09:46 +0200

    added new branch

--- src/modules/add.js
+++ src/modules/add.js
@@ -1,6 +1,7 @@
-import { call, removes, norm } from './edit.js';
-import { status } from './status.js';
+import { call, removes, norm } from "./edit.js";
+import { status } from "./status.js";
 // here is where i created the to do list functions
+const mission = document.getElementById("mission");
 export default class ToDoInfo {
   // created constructor
   constructor(description, ind) {
@@ -15,36 +16,35 @@ export default class ToDoInfo {
 
   // created the add function to html document
   static addToList = (list) => {
-    const mission = document.getElementById('mission');
-    const li = document.createElement('li');
-    const div = document.createElement('div');
-    const checkbox = document.createElement('input');
-    const work = document.createElement('input');
-    const icon = document.createElement('i');
-    icon.setAttribute('class', 'fa-solid fa-trash');
-    div.setAttribute('class', 'none');
-    checkbox.type = 'checkbox';
-    work.type = 'text';
-    work.setAttribute('id', 'edit');
-    checkbox.setAttribute('id', list.index);
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    const checkbox = document.createElement("input");
+    const work = document.createElement("input");
+    const icon = document.createElement("i");
+    icon.setAttribute("class", "fa-solid fa-trash");
+    div.setAttribute("class", "none");
+    checkbox.type = "checkbox";
+    work.type = "text";
+    work.setAttribute("id", "edit");
+    checkbox.setAttribute("id", list.index);
     // added the edit function so it changes in local storage description
-    work.addEventListener('input', () => {
+    work.addEventListener("input", () => {
       call(list.index, work.value);
     });
     // updates the checkboxes in local storage
-    checkbox.addEventListener('input', () => {
+    checkbox.addEventListener("input", () => {
       status(list.index, checkbox.checked);
     });
     // added the show icon
-    li.addEventListener('dblclick', () => {
+    li.addEventListener("dblclick", () => {
       removes(list.index, li, work, div);
     });
     // added the remove icon
-    li.addEventListener('click', () => {
+    li.addEventListener("click", () => {
       norm(list.index, li, work, div);
     });
 
-    checkbox.setAttribute('class', 'checkedbox');
+    checkbox.setAttribute("class", "checkedbox");
     work.value = list.description;
     li.appendChild(checkbox);
     li.appendChild(work);
@@ -55,21 +55,21 @@ export default class ToDoInfo {
   // added the delete function from the list
 
   static deletetask = () => {
-    const retrieveData = localStorage.getItem('list');
+    const retrieveData = localStorage.getItem("list");
     const data = JSON.parse(retrieveData);
-    const checkbox = document.querySelectorAll('.checkedbox');
+    const checkbox = document.querySelectorAll(".checkedbox");
     let count = 0;
     checkbox.forEach((onebyone) => {
       if (onebyone.checked === true) {
         // made sure that the value of check boxs changes the completed in local storage to true
         data[count].completed = onebyone.checked;
         onebyone.parentElement.remove();
-        localStorage.setItem('list', JSON.stringify(data));
+        localStorage.setItem("list", JSON.stringify(data));
         count += 1;
       } else if (onebyone.checked === false) {
         // made sure that the value of check boxs changes the completed in local storage to false
         data[count].completed = onebyone.checked;
-        localStorage.setItem('list', JSON.stringify(data));
+        localStorage.setItem("list", JSON.stringify(data));
         count += 1;
       }
     });
--- src/modules/edit.js
+++ src/modules/edit.js
@@ -1,26 +1,27 @@
 // added the edit function so it changes the local storage description
 const call = (index, value) => {
-  const retrieveData = localStorage.getItem('list');
+  const retrieveData = localStorage.getItem("list");
   const data = JSON.parse(retrieveData);
 
   for (let i = 0; i < data.length; i += 1) {
     if (data[i].index === index) {
       data[i].description = value;
-      localStorage.setItem('list', JSON.stringify(data));
+      localStorage.setItem("list", JSON.stringify(data));
     }
   }
 };
 // when double clicked on the li the icon appears
 const removes = (index, li, work, icon) => {
-  li.style.backgroundColor = 'antiquewhite';
-  work.style.backgroundColor = 'antiquewhite';
-  icon.setAttribute('class', 'show');
-  icon.addEventListener('click', () => {
-    const retrieveData = localStorage.getItem('list');
+  li.style.backgroundColor = "antiquewhite";
+  work.style.backgroundColor = "antiquewhite";
+  icon.setAttribute("class", "show");
+  icon.addEventListener("click", () => {
+    const retrieveData = localStorage.getItem("list");
     const data = JSON.parse(retrieveData);
     for (let i = 0; i < data.length; i += 1) {
-      if (data[i].index === index) {
-        data.splice(i, 1);
+      if (data[i].index !== index) {
+        data = data.filter((data) => data[i]);
+      }
       }
       for (let j = 0; j < data.length; j += 1) {
         // fixes index number of and left list
@@ -28,16 +29,16 @@ const removes = (index, li, work, icon) => {
           data[j].index = j + 1;
         }
       }
-      localStorage.setItem('list', JSON.stringify(data));
+      localStorage.setItem("list", JSON.stringify(data));
       icon.parentElement.remove();
     }
   });
 };
 // when single clicked on li icon disappear
 const norm = (index, li, work, icon) => {
-  li.style.backgroundColor = 'white';
-  work.style.backgroundColor = 'white';
-  icon.setAttribute('class', 'none');
+  li.style.backgroundColor = "white";
+  work.style.backgroundColor = "white";
+  icon.setAttribute("class", "none");
 };
 
 export { call, removes, norm };
